abee667e672bc77e82a5fd7f2838bb50a5d454c6
Jim Hester <james.f.hester@gmail.com>
2015-08-11 13:41:23 -0400

    Do not qualify packages, they are already loaded

--- tests/testthat/test-argufy_package.R
+++ tests/testthat/test-argufy_package.R
@@ -35,24 +35,24 @@
   context("argufy_package")
   test_that("argufy_package works with simple functions", {
 
-    expect_equal(TestS4::a(0), 2)
+    expect_equal(a(0), 2)
 
-    expect_equal(TestS4::a(1), 1)
+    expect_equal(a(1), 1)
 
-    expect_error(TestS4::a("a"), "is.numeric\\(x\\) is not TRUE")
+    expect_error(a("a"), "is.numeric\\(x\\) is not TRUE")
   })
 
   test_that("argufy_package works with S4 generics and methods", {
 
-    expect_equal(TestS4::paste2("a"), "a")
+    expect_equal(paste2("a"), "a")
 
-    expect_equal(TestS4::paste2("a", "b"), "a b")
+    expect_equal(paste2("a", "b"), "a b")
 
     # argufy set on generic
-    expect_error(TestS4::paste2(1), "is.character\\(x\\) is not TRUE")
+    expect_error(paste2(1), "is.character\\(x\\) is not TRUE")
 
     # argufy set on method
-    expect_error(TestS4::paste2("a", 1), "is.character\\(y\\) is not TRUE")
+    expect_error(paste2("a", 1), "is.character\\(y\\) is not TRUE")
   })
 
   dispose_packages(pkgs)
