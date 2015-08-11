ff9c05f54f2a37e025e1f425129f26f2c3c46b4a
Jim Hester <james.f.hester@gmail.com>
2015-08-11 15:53:04 -0400

    Test changes suggested by Gabor

--- tests/testthat/test-argufy_package.R
+++ tests/testthat/test-argufy_package.R
@@ -1,59 +1,62 @@
-{
+library(disposables)
 
-  library(disposables)
-
-  pkgs <- make_packages(
-    TestS4 = {
-
-    a <- function(x = ~ is.numeric) {
-      if (x) {
-        1
-      } else {
-        2
+context("argufy_package")
+test_that("argufy_package works with simple functions", {
+  pkg <- make_packages(
+    simple = {
+      a <- function(x = ~ is.numeric) {
+        if (x) {
+          1
+        } else {
+          2
+        }
       }
-    }
 
-    setGeneric("paste2", function(x = ~ is.character, y) {
-       standardGeneric("paste2")
-      })
-
-    setMethod("paste2",
-      signature(x = "character", y = "missing"),
-      function(x) {
-        paste(x)
-      })
-
-    setMethod("paste2",
-      c(x = "character", y = "ANY"),
-      function(x, y = ~ is.character) {
-        paste(x, y)
-      })
-
-    argufy:::argufy_package("TestS4")
+      argufy::argufy_package()
     })
 
-  context("argufy_package")
-  test_that("argufy_package works with simple functions", {
+  expect_equal(a(0), 2)
 
-    expect_equal(a(0), 2)
+  expect_equal(a(1), 1)
 
-    expect_equal(a(1), 1)
+  expect_error(a("a"), "is.numeric\\(x\\) is not TRUE")
 
-    expect_error(a("a"), "is.numeric\\(x\\) is not TRUE")
-  })
+  dispose_packages(pkg)
+})
 
-  test_that("argufy_package works with S4 generics and methods", {
+test_that("argufy_package works with S4 generics and methods", {
 
-    expect_equal(paste2("a"), "a")
+  pkg <- make_packages(
+    TestS4 = {
 
-    expect_equal(paste2("a", "b"), "a b")
+      setGeneric("paste2", function(x = ~ is.character, y) {
+        standardGeneric("paste2")
+      })
 
-    # argufy set on generic
-    expect_error(paste2(1), "is.character\\(x\\) is not TRUE")
+      setMethod("paste2",
+        signature(x = "character", y = "missing"),
+        function(x) {
+          paste(x)
+        })
 
-    # argufy set on method
-    expect_error(paste2("a", 1), "is.character\\(y\\) is not TRUE")
-  })
+      setMethod("paste2",
+        c(x = "character", y = "ANY"),
+        function(x, y = ~ is.character) {
+          paste(x, y)
+        })
 
-  dispose_packages(pkgs)
-}
+      argufy::argufy_package()
+    })
+
+  expect_equal(paste2("a"), "a")
+
+  expect_equal(paste2("a", "b"), "a b")
+
+  # argufy set on generic
+  expect_error(paste2(1), "is.character\\(x\\) is not TRUE")
+
+  # argufy set on method
+  expect_error(paste2("a", 1), "is.character\\(y\\) is not TRUE")
+
+  dispose_packages(pkg)
+})
