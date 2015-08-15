87e361e4c5d02bd8230c923fc46796e89af229e7
Jim Hester <james.f.hester@gmail.com>
2015-08-15 08:44:05 -0400

    Add tests for getElsewhere

--- R/utils.R
+++ R/utils.R
@@ -3,7 +3,7 @@ getElsewhere <- function(x, y) {
 
   # attached packages from `search()` are prefixed with package:
   # Namespaces from `loadedNamespaces()` are not, so we filter both
-  filtered <- !(found$where %in% c(y, paste0("package:", y)))
+  filtered <- !(found$where %in% paste0(c("", "package:", "namespace:"), y))
 
   if (!any(filtered)) {
     stop(sQuote(x), " not found in un-filtered locations", call. = FALSE)
