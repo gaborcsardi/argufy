4b7f40101d3baa3e01ce2da8604dabf769efa55d
Gabor Csardi <csardi.gabor@gmail.com>
2016-02-01 10:18:58 +0000

    Automatic argufy for packages importing us
    Towards #21, but this is still the old syntax.

---
+++ R/onLoad.R
@@ -0,0 +1,54 @@
+
+## We put in a trace at the beginning of makeLazyLoadDB
+## Note that this function is called (at least) twice during
+## package installation, first for the code R objects,
+## then for the help file R objects. We need to ignore the second one.
+##
+## A cleaner solution would be to trace loadNamespace on exit,
+## but it does not seem to be possible to do that in a robust way,
+## because tracing on exit just calls on.exit(), and that will possibly
+## interfere with the regular on.exit() calls within the function.
+
+tracer_function <- function() {
+  ## Check if a package is being installed
+  instframeno <- find_parent(quote(do_install_source))
+  if (is.na(instframeno)) return()
+
+  ## Check if we are installing the code, as opposed to the help files
+  llframeno <- find_parent(quote(makeLazyLoading))
+  if (is.na(llframeno)) return()
+
+  ## Check if the package uses argufy at all
+  desc <- get("desc", envir = sys.frame(instframeno))
+  imps <- parse_deps(desc["Imports"])
+  if (! "argufy" %in% imps) return()
+
+  ## Find the functions
+  lazyframeno <- find_parent(quote(makeLazyLoadDB))
+
+  fun_env <- get("from", envir = sys.frame(lazyframeno))
+
+  cat("** argufying functions\n")
+  argufy_environment(fun_env)
+
+  invisible()
+}
+  
+.onLoad <- function(libname, pkgname) {
+
+  trace_call <- as.call(list(
+    trace,
+    as.call(list(as.symbol(":::"), quote(tools), quote(makeLazyLoadDB))),
+    print = FALSE,
+    tracer_function
+  ))
+  suppressMessages(eval(trace_call))
+}
+
+.onUnload <- function(path) {
+  untrace_call <- as.call(list(
+    untrace,
+    as.call(list(as.symbol(":::"), quote(tools), quote(makeLazyLoadDB)))
+  ))
+  suppressMessages(eval(untrace_call))
+}
--- R/utils.R
+++ R/utils.R
@@ -10,3 +10,25 @@ getElsewhere <- function(x, y) {
   }
   found$objs[filtered][[1]]
 }
+
+
+find_parent <- function(name) {
+  calls <- sys.calls()
+  for (i in seq_along(calls)) {
+    if (identical(calls[[i]][[1]], name)) return(i)
+  }
+  NA_integer_
+}
+
+
+parse_deps <- function(deps) {
+  deps <- str_trim(strsplit(deps, ",")[[1]])
+  deps <- lapply(strsplit(deps, "\\("), str_trim)
+  deps <- lapply(deps, sub, pattern = "\\)$", replacement = "")
+  vapply(deps, "[", "", 1)
+}
+
+
+str_trim <- function(x) {
+  sub("^\\s+", "", sub("\\s+$", "", x))
+}
